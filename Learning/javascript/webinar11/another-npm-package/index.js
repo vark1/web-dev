9e0cb8e2b8eeed6a79796f3708aa4571085e933a
Varun <varunbhouria78@gmail.com>
2020-05-14 03:09:31 +0530

    sample package created

---
+++ Learning/javascript/webinar11/another-npm-package/index.js
@@ -0,0 +1,4 @@
+let firstPackage = require('../sample-npm-package') 
+//this imports from the 'main' file in sample-npm-package which is index.js specified in package.json
+
+console.log(firstPackage)
---
+++ Learning/javascript/webinar11/sample-npm-package/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    key1: 'val1',
+    key2: 123,
+    key3: [1,2,3]
+}
