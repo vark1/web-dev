be28a03893c7c8d9e7760c175b908240d79fbaba
Varun <varunbhouria78@gmail.com>
2020-05-29 00:07:34 +0530

    login with passport.authenticate()

--- Learning/webinar15&16/passportlogin/server.js
+++ Learning/webinar15&16/passportlogin/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const session = require('express-session')
+const passport = require('./passport')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+app.use(session({
+    secret: 'somesecretstring'
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.set("view engine", "hbs")
 app.set('views',__dirname + '/views')
