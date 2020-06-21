a14ff1a7ea04e88cadaedea27e2ccc4b55e98eac
Varun <varunbhouria78@gmail.com>
2020-06-21 07:22:49 +0530

    basic socketio server

---
+++ Learning/socketio/server.js
@@ -0,0 +1,19 @@
+const http = require('http')           //socketio requires http server to run on
+const express = require('express')
+const app = express()
+const socketio = require('socket.io')
+
+const server = http.createServer(app)  //creating http server
+const io = socketio(server)            //calling socketio on server
+//when i call socketio lib with my http server, it enables certain functionality in my http server. One of them being serving the socketio js file
+//localhost:1635/socket.io/socket.io.js
+
+io.on('connection', (socket) => {
+   console.log('connected with socket id = ', socket.id)
+})
+
+app.use('/',express.static(__dirname + '/public'))
+
+server.listen(1635,()=>{               //same as app.listen
+   console.log("server started at http://localhost:1635")
+})
