f8e6e164c46619301655551b2f8dab8ddf2d1316
Varun <varunbhouria78@gmail.com>
2020-06-26 19:22:50 +0530

    revision: functions (hoisting) and JSON

---
+++ Learning/javascript/jsrevision.js
@@ -0,0 +1,45 @@
+/*
+//functions and function hoisting concept
+
+square_root(10);
+sqrt_n(10); //this does not work here
+
+//function declaration
+function square_root(n){                  //functions defined like this move to the top of the code
+   console.log("In the first sqrt fn");   //so when this function is called before it is defined, it will still run
+   //console.log(Math.sqrt(n));
+   return;
+}
+
+//function expression
+//function hoisting
+var sqrt_n = function(){
+   console.log("In second sqrt function");
+   return;
+}
+*/
+
+
+//Object Oriented Programming in js
+//allows to create objects without using the class
+
+//One way of creating javascript object (JSON) - Javascript Object Notation
+var bird = {
+   x:100,
+   y:10,
+   color:"Blue",
+   eggs:["one","two","three"],
+   fly:function (){
+      console.log("Bird is flying", this.x, this.y)
+   }
+};
+
+//for loop
+for(let i = 0; i < bird.eggs.length; i++){
+   console.log(bird.eggs[i]);
+}
+
+//for each loop
+bird.eggs.forEach(function(val, idx){
+   console.log(idx,val);
+})
