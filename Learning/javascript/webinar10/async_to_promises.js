fa117246fdedd438779d8fb7520e937c72de48ca
Varun <varunbhouria78@gmail.com>
2020-08-04 14:57:55 +0530

    Revision: deferred resolve, async to promise comments

--- Learning/javascript/webinar10/async_to_promises.js
+++ Learning/javascript/webinar10/async_to_promises.js
@@ -1,18 +1,18 @@
-
-function someAsyncTask (callback) {
-    console.log("Beginning of task")
-    setTimeout(function(){
-        console.log("End of task")
-        callback();
-    }, 3000)
+function someAsyncTask(callback) {
+  console.log("Beginning of task");
+  setTimeout(function () {
+    console.log("End of task");
+    callback();
+  }, 3000);
 }
 
-let someTaskPromise = function (){
-    return new Promise(function (resolve, reject){
-        someAsyncTask(resolve)
-    })
-    
-}
-someTaskPromise().then(function(){
-        console.log("After Task is complete")
+let someTaskPromise = function () {
+  //Promisification -> converting async to promise
+  return new Promise(function (resolve, reject) {
+    someAsyncTask(resolve);
+  });
+};
+
+someTaskPromise().then(function () {
+  console.log("After Task is complete");
 });
--- Learning/javascript/webinar10/deferred_resolve.js
+++ Learning/javascript/webinar10/deferred_resolve.js
@@ -1,18 +1,23 @@
-
-function downloadPromise(){
-    return new Promise(function (resolve, reject){
-        console.log("Starting to download the file")
-        setTimeout(function (){
-            console.log("Download is complete")
-            resolve();
-        },3000)
-    })
+function downloadPromise() {
+  return new Promise(function (resolve, reject) {
+    console.log("Starting to download the file");
+    setTimeout(function () {
+      console.log("Download is complete");
+      resolve();
+      //This resolve is called here because ideally when resolve is called, the 'then' function is run.
+      //Promise's state has been changed from pending to resolved
+      //But when the object (here downloadedFile) does not have a 'then', javascript waits until you have a 'then'
+      //We can call 'then' later (ie get the result later) even if the task is completed
+      //this is known as deferred resolve
+    }, 3000);
+  });
 }
 
-let downloadedFile = downloadPromise()
+let downloadedFile = downloadPromise(); //The result is stored in downloaded file
 
-setTimeout(function(){
-    downloadedFile.then(function(){
-        console.log("After Download")
-    })
-},5000)
+setTimeout(function () {
+  downloadedFile.then(function () {
+    //Even after the function has been executed, we can still get the result
+    console.log("After Download");
+  });
+}, 5000);
